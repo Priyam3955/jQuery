fe95e54722f7c75771a777ce32c5f777f88229e2
“Priyam <priyamgupta990@gmail.com>
2023-05-19 20:31:30 +0530

    jQUery

---
+++ Append And Prepend Project/script.js
@@ -0,0 +1,14 @@
+$(() =>{
+    let item = $('#item')
+    let list = $('#list')
+
+    $('#prepend').click(() => {
+        let text = item.val()
+        list.prepend($(`<li>${text}</li>`))
+
+    })
+    $('#append').click(() => {
+        let text = item.val()
+        list.append($(`<li>${text}</li>`))
+    })
+})
